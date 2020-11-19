1769ac2d800e7149facc619d2ca61b759e77f624
mattboan <mattboan97@gmail.com>
2020-11-19 18:58:53 +0800

    Boilerplate stuff

---
+++ front-end/src/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Header.css';
+
+class Header extends React.Component {
+
+    render() {
+        return(
+            <header className="App-header">
+            <a 
+            className="App-header-text" 
+            href="./">
+                Matt Boan
+            </a>
+            <div className="menu">
+            Menu
+            </div>
+        </header>
+        );
+    }
+}
+
+export default Header;
