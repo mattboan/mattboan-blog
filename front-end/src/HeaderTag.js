5b375fe97cc558b90a1fcc10aa845188c7895250
mattboan <mattboan97@gmail.com>
2020-11-20 12:38:55 +0800

    Transfer to PC

---
+++ front-end/src/HeaderTag.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Header.css';
+
+class HeaderTag extends React.Component {
+    
+
+    render() {
+        return(
+            <div className="MainCon">
+                <p></p>
+            </div>
+        );
+    }
+}
+
+export default HeaderTag;
