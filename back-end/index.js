d94ec75666a2eb93de9708c4cb9a7b4b69678c67
mattboan <mattboan97@gmai.com>
2020-11-24 14:13:45 +0800

    Menu implemented

--- back-end/index.js
+++ back-end/index.js
@@ -15,6 +15,7 @@ app.use(bodyParser.json());
 //Temp globals
 const PROJECTS = [
     {
+        id: "001",
         name: "Raspberry Pi Spy Car",
         img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcStCaCObdap38GDLPqKFBihSkRpVWcZxpO9ew&usqp=CAU",
         desc: "Built a little spy car using a raspberry pi 3 and various smaller modules...",
@@ -30,6 +31,7 @@ const PROJECTS = [
         ]
     }, 
     {
+        id: "002",
         name: "MyPlants",
         img: "https://images.pexels.com/photos/4503273/pexels-photo-4503273.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
         desc: "MyPlants is a React Native application that works in conjunction with an ESP32...",
@@ -49,6 +51,12 @@ const PROJECTS = [
 //Get recent projects
 app.get('/projects', (req, res) => {
     res.json(PROJECTS);
+});
+
+//Get a single project
+app.get('/project:id', (req, res) => {
+    //Get the id
+    console.log("/project:id" + req.params.id);
 });
 
 app.listen(port, () => {
--- front-end/src/Header.js
+++ front-end/src/Header.js
@@ -1,21 +1,53 @@
 import React from 'react';
-import { FaBars } from 'react-icons/fa';
+import { FaHome, FaDraftingCompass, FaAddressCard, FaMugHot, FaAd } from 'react-icons/fa';
+import Hamburger from 'hamburger-react';
 import './Header.css';
 
+
 class Header extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            menuOpen: false,
+        };
+        this.onMenuClick = this.onMenuClick.bind(this);
+    }
+
+    onMenuClick() {
+        console.log(this.state.menuOpen);
+        const menuState = this.state.menuOpen;
+        this.setState({ menuOpen: !menuState })
+    }
 
     render() {
         return(
-            <header className="App-header">
-                <a 
-                className="App-header-text" 
-                href="./">
-                    Matt Boan
-                </a>
-                <div className="menu">
-                    <FaBars size="20pt"/>
+            <div>
+                <header className="App-header" >
+                    <a 
+                        className="App-header-text" 
+                        href="./">
+                            Matt Boan
+                    </a>
+                    <Hamburger onToggle={this.onMenuClick} size={26} />
+                </header>
+                <div className={this.state.menuOpen ? 'dropDown slideDown': 'dropDown slideUp'}>
+                    <ul>
+                        <li><a>
+                            <FaHome /> <p>Home</p>
+                        </a></li>
+                        <li><a>
+                            <FaDraftingCompass /> <p>Projects</p>
+                        </a></li>
+                        <li><a>
+                            <FaAddressCard /> <p>About Me</p>
+                        </a></li>
+                        <li><a>
+                            <FaMugHot /> <p>Misc.</p>
+                        </a></li>
+                    </ul>
                 </div>
-            </header>
+            </div>
+            
         );
     }
 }
