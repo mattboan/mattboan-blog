53c9249a4ceb855a9102a5cbe4d250fcb7eb6a26
mattboan <mattboan97@gmail.com>
2020-11-24 09:01:50 +0800

    Passover to PC

--- back-end/index.js
+++ back-end/index.js
@@ -1,34 +1,54 @@
+'use strict';
+
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 //Express init
 const app = express();
 const port = 8080;
 
 //Express middleware
+app.use(cors());
 app.use(bodyParser.json());
 
-//Routes
-app.get('/projects', (req, res) => {
-    const projects = [
-        {
-            name: 'Raspberry Pi Spy Car',
-            desc: '',
-            tags: [
-                {
-                    name: 'C++',
-                    color: 'blue'
-                },
-                {
-                    name: 'Raspberry Pi',
-                    color: 'red',
-                }
-            ]
-        }, 
-    ]; 
+//Temp globals
+const PROJECTS = [
+    {
+        name: "Raspberry Pi Spy Car",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcStCaCObdap38GDLPqKFBihSkRpVWcZxpO9ew&usqp=CAU",
+        desc: "Built a little spy car using a raspberry pi 3 and various smaller modules...",
+        tags: [
+            {
+                color: "#1b8ac2", 
+                text: "C++"
+            },
+            {
+                text: "Raspberry Pi",
+                color: "#bd2b1e",
+            },
+        ]
+    }, 
+    {
+        name: "MyPlants",
+        img: "https://images.pexels.com/photos/4503273/pexels-photo-4503273.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+        desc: "MyPlants is a React Native application that works in conjunction with an ESP32...",
+        tags: [
+            {
+                color: "#61dafb", 
+                text: "React Native"
+            },
+            {
+                text: "ESP32",
+                color: "#e7352c",
+            },
+        ]
+    }
+];
 
-    //Send projects to response
-    res.json(projects[0]);
+//Get recent projects
+app.get('/projects', (req, res) => {
+    res.json(PROJECTS);
 });
 
 app.listen(port, () => {
--- front-end/src/ProjectsCon.js
+++ front-end/src/ProjectsCon.js
@@ -3,56 +3,59 @@ import Project from './Project';
 import BigButton from './BigButton';
 import "./ProjecsCon.css";
 
-const PROJECTS = [
-    {
-        name: "Raspberry Pi Spy Car",
-        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcStCaCObdap38GDLPqKFBihSkRpVWcZxpO9ew&usqp=CAU",
-        desc: "Built a little spy car using a raspberry pi 3 and various smaller modules...",
-        tags: [
-            {
-                color: "#1b8ac2", 
-                text: "C++"
-            },
-            {
-                text: "Raspberry Pi",
-                color: "#bd2b1e",
-            },
-        ]
-    }, 
-    {
-        name: "MyPlants",
-        img: "https://images.pexels.com/photos/4503273/pexels-photo-4503273.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-        desc: "MyPlants is a React Native application that works in conjunction with an ESP32...",
-        tags: [
-            {
-                color: "#61dafb", 
-                text: "React Native"
-            },
-            {
-                text: "ESP32",
-                color: "#e7352c",
-            },
-        ]
-    }
-];
+
 
 class ProjectsCon extends React.Component {
     constructor() {
         super();
+        this.state = {
+            error: null,
+            isLoaded: false,
+            projs: []
+        }
+    }
+
+    componentDidMount() {
+        fetch('http://localhost:8080/projects')
+        .then(res => res.json())
+        .then(
+            (result) => {
+                console.log(result);
+                this.setState({
+                    isLoaded: true,
+                    projs: result
+                });
+            },
+            (error) => {
+                this.setState({
+                    isLoaded: true,
+                    error: error
+                });
+            }
+        );
     }
 
     render() {
-        return(
-            <div className="ProjecCon">
-                <h2>Projects</h2>
-                {PROJECTS.map((project) => 
-                    <Project name={project.name} desc={project.desc} img={project.img} tags={project.tags} />
-                )}
-                <div className="loadmore">
-                    <BigButton text="Load More"></BigButton>
+        const { error, isLoaded, projs } = this.state;
+        
+        //Depending on the state of the fetch request
+        if (error) {
+            return (<div>Error retrieving recent projects. 😕</div>)
+        } else if (!isLoaded) {
+            return (<div>Loading...</div>)
+        } else {
+            return (
+                <div className="ProjecCon">
+                    <h2>Projects</h2>
+                    {
+                        projs.map((project) => <Project name={project.name} desc={project.desc} img={project.img} tags={project.tags} /> ) 
+                    }
+                    <div className="loadmore">
+                        <BigButton text="Load More"></BigButton>
+                    </div>
                 </div>
-            </div>
-        );
+            );
+        }
     }
 }
 
