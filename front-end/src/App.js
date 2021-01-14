4dd8e8b88b9c8efc5d30e0b316b89ee2a7c56381
mattboan <mattboan97@gmai.com>
2021-01-14 09:57:48 +0800

    Fixed scrolling issue for router

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 //Components
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
+import ScrollToTop from "./Components/ScrollToTop";
 
 //Views
 import Home from "./Views/Home";
@@ -15,35 +16,39 @@ import EditProject from "./Views/EditProject";
 function App() {
     return (
         <Router>
-            <div>
-                <div className="AppContent">
-                    <div className="HeaderWrapper">
-                        <Header></Header>
+            <ScrollToTop>
+                <div>
+                    <div className="AppContent">
+                        <div className="HeaderWrapper">
+                            <Header></Header>
+                        </div>
+                        <div className="dynamicCon">
+                            <Switch>
+                                <Route
+                                    path="/EditProject/:id"
+                                    render={(props) => (
+                                        <EditProject {...props} />
+                                    )}
+                                />
+                                <Route path="/Projects">
+                                    <Projects />
+                                </Route>
+                                <Route
+                                    path="/Project/:id"
+                                    render={(props) => <Project {...props} />}
+                                ></Route>
+                                <Route path="/AboutMe">
+                                    <AboutMe />
+                                </Route>
+                                <Route path="/">
+                                    <Home />
+                                </Route>
+                            </Switch>
+                        </div>
                     </div>
-                    <div className="dynamicCon">
-                        <Switch>
-                            <Route
-                                path="/EditProject/:id"
-                                render={(props) => <EditProject {...props} />}
-                            />
-                            <Route path="/Projects">
-                                <Projects />
-                            </Route>
-                            <Route
-                                path="/Project/:id"
-                                render={(props) => <Project {...props} />}
-                            ></Route>
-                            <Route path="/AboutMe">
-                                <AboutMe />
-                            </Route>
-                            <Route path="/">
-                                <Home />
-                            </Route>
-                        </Switch>
-                    </div>
+                    <Footer></Footer>
                 </div>
-                <Footer></Footer>
-            </div>
+            </ScrollToTop>
         </Router>
     );
 }
