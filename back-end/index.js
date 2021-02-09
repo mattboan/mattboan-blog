486683c358b573bcd9efd98f740753af8840aef2
Matthew boan <mattboan97@gmail.com>
2021-02-09 14:59:46 +0800

    Added creds

--- back-end/index.js
+++ back-end/index.js
@@ -26,9 +26,24 @@ const auth = require("./middleware/auth");
 var bouncer = require("express-bouncer")(12000, 900000); //min=2min max=10min
 
 //HTTPS Config
-const privateKey = fs.readFileSync("key/server.key", "utf8");
-const certificate = fs.readFileSync("key/server.cert", "utf8");
-const credentials = { key: privateKey, cert: certificate };
+const privateKey = fs.readFileSync(
+	"/etc/letsencrypt/live/www.mattboan.com/privkey.pem",
+	"utf8"
+);
+const certificate = fs.readFileSync(
+	"/etc/letsencrypt/live/www.mattboan.com/cert.pem",
+	"utf8"
+);
+const ca = fs.readFileSync(
+	"/etc/letsencrypt/live/www.mattboan.com/chain.pem",
+	"utf8"
+);
+
+const credentials = {
+	key: privateKey,
+	cert: certificate,
+	ca: ca,
+};
 
 const saltRounds = 10;
 
