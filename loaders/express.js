db21b9b6fd0880f93c6c491a6097d893b5aa987d
Matthew boan <mattboan97@gmail.com>
2021-02-12 18:45:26 +0800

    HTTPS redirect working

--- loaders/express.js
+++ loaders/express.js
@@ -30,11 +30,9 @@ module.exports = async (app) => {
 	try {
 		app.use(function (req, res, next) {
 			if (req.secure) {
-				// request was via https, so do no special handling
 				next();
 			} else {
-				// request was via http, so redirect to https
-				res.redirect("https://" + req.headers.host + req.url);
+				res.redirect("https://" + req.headers.host);
 			}
 		});
 		console.log("\t✅ " + "HTPS Reroute middleware added".green);
