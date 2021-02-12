f52fe4edf0c0a0022ed58ed9e23f3d53f28227f2
Matthew boan <mattboan97@gmail.com>
2021-02-12 15:43:09 +0800

    url changes

--- config/URL.js
+++ config/URL.js
@@ -1,6 +1,6 @@
 const URL = {
-	backend: "http://192.168.0.120:8080",
-	frontend: "http://192.168.0.120:3000",
+	backend: "https://www.mattboan.com",
+	frontend: "https://www.mattboan.com",
 };
 
 module.exports = URL;
--- front-end/src/Config/URL.js
+++ front-end/src/Config/URL.js
@@ -1,5 +1,5 @@
 const URL = {
-	backend: "http://192.168.0.120:8080",
+	backend: "https://www.mattboan.com",
 	frontend: "https://www.mattboan.com",
 };
 
